587f9820f1407865746dda297c09a4358ca5ad91
Ing. Facundo Montiel <ingfacundomontiel@gmail.com>
2024-10-23 11:25:03 -0300

    Enqueue Assets: Conditionally for Single Classifieds or Shortcodes

--- functions/enqueue-assets.php
+++ functions/enqueue-assets.php
@@ -10,12 +10,26 @@
  *
  * @return void
  */
-function classifieds_plugin_enqueue_styles() {
+function classifieds_plugin_enqueue_styles()
+{
 
-	wp_enqueue_script( 'bootstrap', plugin_dir_url( __FILE__ ) . '../dist/bootstrap.min.js', array( 'jquery' ), '5.0.0', true ); // Enqueue Bootstrap JS.
-	wp_enqueue_script( 'main', plugin_dir_url( __FILE__ ) . '../dist/main.min.js', array( 'jquery' ), '5.0.0', true ); // Enqueue Bootstrap JS.
+	if ( is_singular('classified' )) {
+		wp_enqueue_script('bootstrap', plugin_dir_url(__FILE__) . '../dist/bootstrap.min.js', array('jquery'), '5.0.0', true); // Enqueue Bootstrap JS.
+		wp_enqueue_script('main', plugin_dir_url(__FILE__) . '../dist/main.min.js', array('jquery'), '5.0.0', true); // Enqueue Bootstrap JS.
 
-	wp_enqueue_style( 'bootstrap', plugin_dir_url( __FILE__ ) . '../dist/bootstrap.min.css' ); // Enqueue Bootstrap CSS.
-	wp_enqueue_style( 'classifieds-styles', plugin_dir_url( __FILE__ ) . '../dist/main.min.css' ); // Enqueue the plugin's CSS file.
+		wp_enqueue_style('bootstrap', plugin_dir_url(__FILE__) . '../dist/bootstrap.min.css'); // Enqueue Bootstrap CSS.
+		wp_enqueue_style('classifieds-styles', plugin_dir_url(__FILE__) . '../dist/main.min.css'); // Enqueue the plugin's CSS file.
+	}
+
+	// Check if the page contains the classifieds shortcode.
+    global $post;
+
+    if ( isset($post->post_content ) && ( has_shortcode( $post->post_content, 'classifieds_list' )  || has_shortcode( $post->post_content, 'classified_form' ) ) ) {
+        wp_enqueue_script('bootstrap', plugin_dir_url(__FILE__) . '../dist/bootstrap.min.js', array('jquery'), '5.0.0', true); // Enqueue Bootstrap JS.
+		wp_enqueue_script('main', plugin_dir_url(__FILE__) . '../dist/main.min.js', array('jquery'), '5.0.0', true); // Enqueue Bootstrap JS.
+
+		wp_enqueue_style('bootstrap', plugin_dir_url(__FILE__) . '../dist/bootstrap.min.css'); // Enqueue Bootstrap CSS.
+		wp_enqueue_style('classifieds-styles', plugin_dir_url(__FILE__) . '../dist/main.min.css'); // Enqueue the plugin's CSS file.
+    }
 }
-add_action( 'wp_enqueue_scripts', 'classifieds_plugin_enqueue_styles' ); // Hook the styles to be enqueued on the frontend.
+add_action('wp_enqueue_scripts', 'classifieds_plugin_enqueue_styles'); // Hook the styles to be enqueued on the frontend.
